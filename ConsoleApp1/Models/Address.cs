8ac9e2e86d7c3f71f361222a1cdbffcbaaab5383
AxelSantamariaR <133828324+AxelSantamariaR@users.noreply.github.com>
2023-07-05 22:42:05 -0500

    Control de Transacciones en la BD

---
+++ ConsoleApp1/Models/Address.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Models
+{
+    public class Address
+    {
+        public int AddressId { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+    }
+}
